e802662ca300a168dc59aaa66d15c888aa6a4741
glaszig <glaszig@gmail.com>
2014-09-19 03:12:51 +0200

    applying appearance

--- iOS8 UITableView backgroundColor/AppDelegate.m
+++ iOS8 UITableView backgroundColor/AppDelegate.m
@@ -17,7 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    [[UITableView appearance] setBackgroundColor:[UIColor blueColor]];
+
     return YES;
 }
 
